526c0ce79a81a75d5206f94c57b3ea9bc395fb8a
bobyin <bobyin22@gmail.com>
2024-06-05 13:41:26 +0800

    Day36 建立project8 專案，Device 設定限制只用 iPAD

---
+++ project8/project8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project8
+//
+//  Created by 邱慧珊 on 2024/6/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

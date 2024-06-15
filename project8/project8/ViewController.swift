063996a472d41b8f4ef1fe896893eb2cd298376d
bobyin <bobyin22@gmail.com>
2024-06-15 11:18:25 +0800

    [add] Day37 topic1 loadingLevel，讀取txt文字檔資料，切分整理並顯示在畫面上 • 把文字依據 “\n” 整理成陣列 • 依據 ": " 整理，轉給下方按鈕 • Hint 給左方cluesLabel，字數給右方answerLabel

--- project8/project8/ViewController.swift
+++ project8/project8/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var cluesLabel: UILabel!            //右
     var answerLabel: UILabel!           //左
     var currentAnswer: UITextField!     //中
     var scoreLabel: UILabel!            //右上
     var letterButtons = [UIButton]()    //下
+    
+    var activatedButtons = [UIButton]() //存儲使用者點擊的拼字
+    var solutions = [String]()
+    
+    var score = 0
+    var level = 1
     
     override func loadView() {
         view = UIView()
@@ -58,12 +64,14 @@ class ViewController: UIViewController {
         let submitBtn = UIButton(type: .system)
         submitBtn.translatesAutoresizingMaskIntoConstraints = false
         submitBtn.setTitle("SUBMIT", for: .normal)
+        submitBtn.addTarget(self, action: #selector(submitTapped), for: .touchUpInside)
         view.addSubview(submitBtn)
         
         //中 右 (不當作屬性)
         let clearBtn = UIButton(type: .system)
         clearBtn.translatesAutoresizingMaskIntoConstraints = false
         clearBtn.setTitle("CLEAR", for: .normal)
+        clearBtn.addTarget(self, action: #selector(clearTapped), for: .touchUpInside)
         view.addSubview(clearBtn)
         
         //下 (不當作屬性)
@@ -111,6 +119,7 @@ class ViewController: UIViewController {
                 let letterButton = UIButton(type: .system)
                 letterButton.titleLabel?.font = UIFont.systemFont(ofSize: 36)
                 letterButton.setTitle("WWW", for: .normal)
+                letterButton.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
                 let frame = CGRect(x: column * width, y: row * height, width: width, height: height)
                 letterButton.frame = frame
                 
@@ -126,9 +135,136 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loadLevel()
+    }
+    
+    @objc func letterTapped(_ sender: UIButton) {
+        
+    }
+    
+    @objc func submitTapped(_ sender: UIButton) {
+        
+    }
+    
+    @objc func clearTapped(_ sender: UIButton) {
+        
     }
 
-
+    
+    func loadLevel() {
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+        
+        /*
+         levelFileURL是：取得檔案url位置
+         file:///Users/joy/Library/Developer/CoreSimulator/Devices/6802363F-84F6-4E0C-A3FF-2196E5CE575E/data/Containers/Bundle/Application/58C207E5-1E06-4FEC-8FC7-0C0B31D20847/project8.app/level1.txt
+         */
+        if let levelFileURL = Bundle.main.url(forResource: "level\(level)", withExtension: "txt") {
+            
+            /*
+             levelContents是：把url轉成String
+             Optional("HA|UNT|ED: Ghosts in residence\nLE|PRO|SY: A Biblical skin disease\nTW|ITT|ER: Short but sweet online chirping\nOLI|VER: Has a Dickensian twist\nELI|ZAB|ETH: Head of state, British style\nSA|FA|RI: The zoological web\nPOR|TL|AND: Hipster heartland")
+             */
+            if let levelContents = try? String(contentsOf: levelFileURL) {
+                
+                /*
+                 lines是：把字串根據"\n"切成，陣列
+                 ["HA|UNT|ED: Ghosts in residence", "LE|PRO|SY: A Biblical skin disease", "TW|ITT|ER: Short but sweet online chirping", "OLI|VER: Has a Dickensian twist", "ELI|ZAB|ETH: Head of state, British style", "SA|FA|RI: The zoological web", "POR|TL|AND: Hipster heartland"]
+                 */
+                var lines = levelContents.components(separatedBy: "\n")
+                
+                
+                /*
+                 ["OLI|VER: Has a Dickensian twist", "LE|PRO|SY: A Biblical skin disease", "POR|TL|AND: Hipster heartland", "TW|ITT|ER: Short but sweet online chirping", "SA|FA|RI: The zoological web", "ELI|ZAB|ETH: Head of state, British style", "HA|UNT|ED: Ghosts in residence"]
+                 */
+                lines.shuffle()
+                
+                //全部走一次陣列，從index 0，line是 lines[0]
+                for (index, line) in lines.enumerated() {
+                    /*
+                     index 0
+                     line "OLI|VER: Has a Dickensian twist"
+                     */
+                    
+                    /*
+                     parts是：把字串根據": "切成，陣列
+                     ["OLI|VER", "Has a Dickensian twist"]
+                     */
+                    let parts = line.components(separatedBy: ": ")
+                    /*
+                     answer是: "OLI|VER"
+                     */
+                    let answer = parts[0]
+                    
+                    /*
+                     clue是：Has a Dickensian twist
+                     */
+                    let clue = parts[1]
+                    
+                    /*
+                     clueString是：
+                     "1. Has a Dickensian twist\n"
+                     */
+                    clueString += "\(index + 1). \(clue)\n"
+                    
+                    /*
+                     solutionWord是：OLIVER
+                     */
+                    let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                    
+                    /*
+                     solutionString是：
+                     "6 letters\n"
+                     */
+                    solutionString += "\(solutionWord.count) letters\n"
+                    
+                    /*
+                     solutions是：
+                     ["OLIVER"]
+                     */
+                    solutions.append(solutionWord)
+                    
+                    /*
+                     bits是：
+                     ["OLI", "VER"]
+                     */
+                    let bits = answer.components(separatedBy: "|")
+                    
+                    /*
+                     letterBits是：
+                     ["OLI", "VER"]
+                     */
+                    letterBits += bits              //for (index, line) in lines.enumerated() 所有迴圈走完，會變成陣列有20個元素，["OLI", "VER", "HA", "UNT, "ED", .......]
+                }
+            }
+        }
+        
+        /*
+         cluesLabel.text是：把\n去掉
+         "1. Has a Dickensian twist\n" (舊)
+         1. Has a Dickensian twist (新)
+         */
+        cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        /*
+         answerLabel.text是：
+         6 letters
+         */
+        answerLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        //把下方按鈕UI位置重洗
+        letterButtons.shuffle()
+        
+        
+        //當for (index, line) in lines.enumerated() 所有迴圈走完，會變成陣列有20個元素，["OLI", "VER", "HA", "UNT, "ED", .......]
+        if letterButtons.count == letterBits.count {
+            for i in 0..<letterButtons.count {
+                letterButtons[i].setTitle(letterBits[i], for: .normal)
+            }
+        }
+    }
     
 }
 

86ecb2ecdef055902691000dad2202c60667186f
bobyin <bobyin22@gmail.com>
2024-09-13 12:02:49 +0800

    [add] Storyboard 建立VC是CollectionViewController 內有PersonCell imageView與Label

---
+++ project10/project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  project10
+//
+//  Created by Yin Bob on 2024/9/13.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
---
+++ project10/project10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project10
+//
+//  Created by Yin Bob on 2024/9/13.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

1e08c2c550eb3d142c2e05c1b6dd8c111deb18aa
bobyin <bobyin22@gmail.com>
2024-09-13 14:50:01 +0800

    [modify] D43 topic 2 建立Person資料型別，把拿到照片資料存入

---
+++ project10/project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  project10
+//
+//  Created by Yin Bob on 2024/9/13.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var naem: String
+    var image: String
+    
+    init(naem: String, image: String) {
+        self.naem = naem
+        self.image = image
+    }
+    
+}
--- project10/project10/ViewController.swift
+++ project10/project10/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var people = [Person]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
@@ -45,6 +47,10 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         if let jpegData = image.jpegData(compressionQuality: 0.8) {
             try? jpegData.write(to: imagePath)
         }
+        
+        let person = Person(naem: "沒有名字", image: imageName)
+        people.append(person)
+        collectionView.reloadData()
         
         dismiss(animated: true)
     }

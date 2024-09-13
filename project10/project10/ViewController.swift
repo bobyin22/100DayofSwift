799b9b01232d5f469bd89c5a7fafae37ed250e45
bobyin <bobyin22@gmail.com>
2024-09-13 14:17:17 +0800

    [modify] D42 topic 3 建立CollectionView的 data source

--- project10/project10/ViewController.swift
+++ project10/project10/ViewController.swift
@@ -11,9 +11,19 @@ class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemMint
     }
-
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell.")
+        }
+        return cell
+    }
 
 }
 

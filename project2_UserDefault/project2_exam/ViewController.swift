ef69cb707125d5dd2f250ae03e2fbe5e6a3bde83
bobyin <bobyin22@gmail.com>
2024-10-07 08:24:32 +0800

    [modify] 遊戲結束存UserDeafault - bug 第一次存不了，因為沒有找到Key

--- project2_UserDefault/project2_exam/ViewController.swift
+++ project2_UserDefault/project2_exam/ViewController.swift
@@ -12,24 +12,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
-    
-    
     @IBOutlet weak var barButton: UIBarButtonItem!
-    
-    
-    @IBAction func tapBarButtonItem(_ sender: UIBarButtonItem) {
-        barButton.title = String(score)
-    }
-    
-    
     
     var flagArray: [String] = ["estonia", "france", "germany", "ireland", "italy", "nigeria", "poland", "russia", "spain", "uk", "us"]
     var quizz: String?
     var randomCountry: Int?     //創建隨機數量，隨機國家當作答案
     var score = 0
     var testlimit = 10
-    
     var isGuessRight: Bool?
+    let defaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        barButton.title = "看分數"
+        view.backgroundColor = .systemGray4
+        button1.layer.borderWidth = 1.0
+        button1.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
+        button2.layer.borderWidth = 1.0
+        button2.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
+        button3.layer.borderWidth = 1.0
+        button3.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
+        setupQuesionUI()
+        getWhereUserDefaultFile()
+    }
+    
+    
+    
+    @IBAction func tapBarButtonItem(_ sender: UIBarButtonItem) {
+        barButton.title = String(score)
+    }
     
     //如果選對，分數+1，玩的次數-1，猜國家題目畫面要變動
     //如果選錯，分數-1，玩的次數-1，猜國家題目畫面不變
@@ -56,6 +67,12 @@ class ViewController: UIViewController {
     func checkBtnStatus() {
         if testlimit == 0 {
             popStopMessage()
+            //存UserDefault
+            guard let userDefaultsHighestScore = defaults.array(forKey: "HighestScore") as? Int ?? nil else { return }
+            //如果目前分數有比UserDefault大，要更新UserDefault值
+            if score > userDefaultsHighestScore || userDefaultsHighestScore == nil{
+                defaults.set(score, forKey: "HighestScore")
+            }
         } else {
             calculatePlusSocreAndPlayTime()
             popRightMessage()
@@ -71,21 +88,6 @@ class ViewController: UIViewController {
     func calculateMinusScoreAndPlayTime() {
         score -= 1
         testlimit -= 1
-    }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        barButton.title = "看分數"
-        view.backgroundColor = .systemGray4
-        button1.layer.borderWidth = 1.0
-        button1.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
-        button2.layer.borderWidth = 1.0
-        button2.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
-        button3.layer.borderWidth = 1.0
-        button3.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 1)
-        
-        setupQuesionUI()
     }
     
     func popRightMessage() {
@@ -122,6 +124,24 @@ class ViewController: UIViewController {
         button1.setImage(UIImage(named: flagArray[0]), for: .normal)
         button2.setImage(UIImage(named: flagArray[1]), for: .normal)
         button3.setImage(UIImage(named: flagArray[2]), for: .normal)
+    }
+    
+    func getWhereUserDefaultFile() {
+        // 獲取應用程序的沙盒目錄
+        if let applibraryDDir = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask).first {
+                    
+              // 拼接 Preferences 子目錄
+              let preferencesDir = applibraryDDir.appendingPathComponent("Preferences")
+                    
+              // 檢查 Preferences 目錄是否存在
+              if FileManager.default.fileExists(atPath: preferencesDir.path) {
+                    print("UserDefaults 存儲位置：\(preferencesDir.path)")
+              } else {
+                    print("UserDefaults 存儲位置未找到")
+              }
+         } else {
+            print("無法獲取應用程序沙盒目錄")
+         }
     }
 
 }

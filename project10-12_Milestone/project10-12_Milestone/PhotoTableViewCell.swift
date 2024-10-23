9c96b70f076f833035270d109c37a324d09bd16d
bobyin <bobyin22@gmail.com>
2024-10-23 13:47:59 +0800

    [modify] 調整Cell 內元件UI，左邊是Image 右邊是Label

--- project10-12_Milestone/project10-12_Milestone/PhotoTableViewCell.swift
+++ project10-12_Milestone/project10-12_Milestone/PhotoTableViewCell.swift
@@ -22,5 +22,13 @@ class PhotoTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    // Cell內元素內縮
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        //set the values for top,left,bottom,right margins
+        let margins = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+        contentView.frame = contentView.frame.inset(by: margins)
+    }
 
 }
